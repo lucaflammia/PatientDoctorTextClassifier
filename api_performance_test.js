5710c8bf4c80c4c68d00f7cd0b196ed6d8d447b6
lucaflammia <luca.flammia@gmail.com>
2024-03-01 15:18:29 +0100

    first commit

---
+++ api_performance_test.js
@@ -0,0 +1,31 @@
+import http from 'k6/http';
+import { check, group, sleep } from "k6";
+
+export let options = {
+  stages: [
+    { duration: '10m', target: 100 }, // simulate ramp-up of traffic 
+    { duration: '30m', target: 100 }, // stay at a certain value of traffic
+    { duration: '5m', target: 0 }, // ramp-down
+  ]
+};
+
+export default function () {
+  group("v1 API testing", () => {
+    var url = 'https://19v5ai4stg.execute-api.eu-west-1.amazonaws.com/SagemakerInvokeEndpointAPIStage/SagemakerInvokeEndpointAPIResource';
+    var payload = JSON.stringify({
+      "text": "I prescribe some drugs I force you to stay at home for a week."
+    });
+    var params = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    group('Make predictions about input texts', () => {
+      const res = http.post(url, payload, params);
+      check(res, {
+        "is status 200": (r) => r.status == 200
+      });
+    });
+  });
+  sleep(1)
+}
