387d7b7ffaf556f229e39e755e995e56cde9cde6
lucaflammia <luca.flammia@gmail.com>
2024-05-29 08:47:38 +0200

    add medical bio terms for better inferences

--- ec2_script.py
+++ ec2_script.py
@@ -11,18 +11,18 @@ import os
 HF_USERNAME = "LukeGPT88"
 PROJECT_NAME = "patient-doctor-text-classifier"
 SUB_PROJECT_NAME = "eng"
-TASK = f"{PROJECT_NAME}-{SUB_PROJECT_NAME}-0523"
+TASK = f"{PROJECT_NAME}-{SUB_PROJECT_NAME}-0528"
 
 FOLDERPATH = 'PulsarSearchesExport'
-TOPIC_CC = 'HavasGlobal-Lupus'
-TOPIC_SC = 'havas_global-lupus'
+TOPIC_CC = 'BreastCancer'
+TOPIC_SC = 'breast_cancer'
 
 def get_data(df):
-    contents = df['content'].values
-    bios = df['bio'].values
-    data = {'contents': contents, 'bios': bios}
-    # bios = bios.where(bios.notna(), bios, None).values
-    return data
+  contents = df['content'].values
+  bios = df['bio'].values
+  data = {'contents': contents, 'bios': bios}
+  # bios = bios.where(bios.notna(), bios, None).values
+  return data
 
 def get_csv_file(data, check_bios, split=0):
   out_df = pd.DataFrame({
@@ -64,7 +64,7 @@ def get_classification(df, check_bios=False):
 
   classifier = pipeline("text-classification", model=f"{HF_USERNAME}/{TASK}")
   outcomes = []
-  tot_rows = list(map(lambda x, w, y, z: (x, w, y, z), df['user name'].values,df['user screen name'].values, data['contents'], data['bios']))
+  tot_rows = list(map(lambda x, w, y, z: (x, w, y, z), df['user name'].values, df['user screen name'].values, df['content'].values, df['bio'].values ))
   for i, (account, user, content, bio) in enumerate(tot_rows):
     meaningful_content = get_meaningful_text(content)
     if len(meaningful_content) <= 500:
@@ -75,24 +75,24 @@ def get_classification(df, check_bios=False):
       outcomes.append({"account": account, "user": user, "content": {**{"text": content}, **classifier(meaningful_content)[0]}, "bio": {**{"text": bio}, **classifier(meaningful_bio)[0]}})
     if i%1000 == 0 and i != 0:
       split = i/1000
-      get_csv_file(outcomes, split, check_bios)
+      get_csv_file(outcomes, check_bios, split)
     if i == len(tot_rows) - 1:
       get_csv_file(outcomes, check_bios)
 
 def classify_medical_bios(df):
   # List of common medical terms or keywords
   medical_terms = [
-    'doctor', 'phd',  'nurse', 'hospital', 'clinic', 'medicine', 'surgery', 'pharmacy',
-    'treatment', 'diagnosis', 'therapy', 'health', 'disease', 'condition',
-    'symptom', 'patient', 'medical', 'prescription', 'vaccine', 'infection',
-    'cardiology', 'dermatology', 'neurology', 'oncology', 'pathology', 'radiology', 'cancer', 'lupus'
+    'doctor', 'phd', 'professor', 'biotech',  'nurse', 'hospital', 'clinic', 'medicine', 'surgery', 'pharmacy',
+    'treatment', 'diagnosis', 'vet', 'therapy', 'health', 'disease', 'condition', 'rheumatologist', 'md',
+    'symptom', 'patient', 'medical', 'prescription', 'vaccine', 'infection', 'healtcare',
+    'cardiology', 'dermatology', 'neurology', 'kidney', 'breast', 'oncology', 'pathology', 'radiology', 'cancer', 'lupus'
   ]
 
   # Create a regex pattern from the list of medical terms
   pattern = re.compile(r'\b(?:' + '|'.join(medical_terms) + r')\b', re.IGNORECASE)
   check_bio = []
   data = get_data(df)
-  for bio in data['bios'][:10]:
+  for bio in data['bios']:
     if type(bio) != str or bio == '':
       continue
     for word in bio.lower().split(' '):
@@ -100,8 +100,7 @@ def classify_medical_bios(df):
         check_bio.append(bio)
   
   df = df.loc[df['bio'].isin(check_bio) == True]
-  print(df)
-  get_classification(df, check_bios=True)
+  df.to_csv('medical_bios_df.csv')
 
 # Function to check if a word contains any reference to the medical world
 def contains_medical_reference(word, pattern):
@@ -114,11 +113,10 @@ if __name__ == '__main__':
     args = parser.parse_args()
     start_time = time.time()
     
-    df = pd.read_excel(f'{FOLDERPATH}/{TOPIC_CC}/107503-all-2024-05-17-07-39-05-1163478.xlsx')
+    df = pd.read_excel(f'{FOLDERPATH}/{TOPIC_CC}/74982-all-2024-05-17-07-08-08-1163476.xlsx')
     df.drop_duplicates(subset=['content'], inplace=True)
     df.dropna(subset=['content'], inplace=True)
 
-    data = get_data(df)
     if 'classification' in parser.parse_args().ops:
         get_classification(df)
     elif 'medical_bios' in parser.parse_args().ops:
